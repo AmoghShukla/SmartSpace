10ba842bbaf49630773dbc07f723e7825794a524
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-27 16:00:30 +0530

    added logging utility and database configuration
    - Introduced a logging utility in src/utils/loggers.py to handle logging with both console and file outputs.
    - Added a configuration class in src/core/config.py using pydantic-settings for environment variable management.
    - Created a base class for SQLAlchemy models in src/database/Base.py.
    - Set up session management in src/database/Session.py with SQLAlchemy.
    - Updated pyproject.toml to include sqlalchemy and pydantic-settings as dependencies.

---
+++ src/database/Session.py
@@ -0,0 +1,3 @@
+from sqlalchemy.orm import create_session
+from sqlalchemy import create_engine
+
---
+++ src/utils/loggers.py
@@ -0,0 +1,22 @@
+import logging
+import os
+
+
+def get_logger(name : str):
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if logger.hasHandlers():
+        if not os.path.exists('src/logs'):
+            os.makedirs('src/logs')
+        
+        formatter = logging.Formatter(' %(asctime)s | %(levelname)s | %(name)s | %(message)s ')
+
+        ConsoleHandler = logging.StreamHandler()
+        ConsoleHandler.setFormatter(formatter)
+
+        FileHandler = logging.FileHandler('src/logs/app.log')
+        FileHandler.setFormatter(formatter)
+    
+    return logger
